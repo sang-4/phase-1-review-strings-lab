b2360b4f6b7fe83a504bacd93f91789fd861c2ba
Allan <kipropsallan@gmail.com>
2022-06-08 18:45:23 +0300

    commit tree 0330db0a5effedd67fe93ad9ed0c37ce7985e9e4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+//define currentUser
+let currentUser= "Allan";
+//welcome message to include variable
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+//excitedWelcomeMessage
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+//
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
